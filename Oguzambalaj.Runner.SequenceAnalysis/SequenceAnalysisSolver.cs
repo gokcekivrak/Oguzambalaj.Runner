c75ae1796d60c7e730d004fa3bce630a83ccef10
gokcekivrak <gkce12@gmail.com>
2021-10-10 12:15:49 +0300

    Proje dosyası ekle.

---
+++ Oguzambalaj.Runner.SequenceAnalysis/SequenceAnalysisSolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Oguzambalaj.Runner.SequenceAnalysis
+{
+    public class SequenceAnalysisSolver
+    {
+        private const int MaxChar = 26;
+
+        public string Solve(string input)
+        {
+            var punctuation = input.Where(char.IsPunctuation).Distinct().ToArray();
+            var words = input.Split().Select(x => x.Trim(punctuation));
+         
+            var charCount = new int[MaxChar];
+            foreach (var word in words)
+            {
+                if (word != word.ToUpper()) continue;
+
+                
+                char[] charsToTrim = { '*', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                var asciiBytes = Encoding.ASCII.GetBytes(word.Trim(charsToTrim).Replace("'", string.Empty));
+
+               
+                foreach (var letter in asciiBytes)
+                {
+                    if (letter < 65 || letter > 90)
+                        throw new InvalidOperationException($"Input contains non-ascii character '{(char)letter}'. Non-Ascii characters are not supported");
+
+                    charCount[letter % 65]++;
+                }
+            }
+
+            var result = new StringBuilder();
+            
+            for (var i = 0; i < MaxChar; i++)
+                result.Append(new string((char)(i + 65), charCount[i]));
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Oguzambalaj.Runner.SumOfMultiple/SumOfMultipleSolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Oguzambalaj.Runner.SumOfMultiple
+{
+    public class SumOfMultipleSolver
+    {
+
+        public ulong Solve(int input)
+        {
+            if (input <= 0)
+                throw new ArgumentOutOfRangeException(nameof(input), input, "Limit needs to be positive number!");
+            var limit = (ulong)(input - 1);
+           
+            var sumOf3 = SumOfAllNumbersMultipleOfXTillN(3, limit);
+            
+            var sumOf5 = SumOfAllNumbersMultipleOfXTillN(5, limit);
+
+            
+            var sumOf15 = SumOfAllNumbersMultipleOfXTillN(15, limit);
+
+          
+            return sumOf3 + sumOf5 - sumOf15;
+        }
+
+        private static ulong SumOfAllNumbersMultipleOfXTillN(ulong multipleOf, ulong limit)
+        {
+            return SumOfAllNumbersTillN(limit / multipleOf) * multipleOf;
+        }
+
+        private static ulong SumOfAllNumbersTillN(ulong n)
+        {
+            return n * (n + 1) / 2;
+        }
+    }
+}
